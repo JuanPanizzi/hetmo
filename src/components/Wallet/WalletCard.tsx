90f02fd66064ab11a38cc2aee0a3803bfe29e185
Juan <juanpanizzi@hotmail.com>
2025-04-18 14:41:15 -0300

    adv remove wallet

--- src/components/Wallet/WalletCard.tsx
+++ src/components/Wallet/WalletCard.tsx
@@ -12,7 +12,7 @@ export default function WalletCard({wallet}: {wallet: Wallet}) {
     const header = (
         <div className='p-1'>
 
-      <h1>{wallet.name}</h1>
+      <h1 className='text-2xl font-bold'>{wallet.name}</h1>
         </div>
         // <img alt="Card" src="https://primefaces.org/cdn/primereact/images/usercard.png" />
     );
@@ -20,6 +20,7 @@ export default function WalletCard({wallet}: {wallet: Wallet}) {
         <>
             <Button  icon="pi pi-check" />
             <Button  severity="danger" icon="pi pi-trash" style={{ marginLeft: '0.5em' }} onClick={() => deleteWallet(Number(wallet.id))} />
+            
         </>
     );
 
@@ -28,10 +29,21 @@ export default function WalletCard({wallet}: {wallet: Wallet}) {
     return (
         <div className="card flex justify-content-center xl:max-w-xl">
             <Card title="" subTitle="" footer={footer} header={header} className="">
-                <p className="m-0">
-                    {/* Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
-                    numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas! */}
-                </p>
+                {
+                    !wallet.cryptocurrencies?.length ? (
+                        <p>No hay criptomonedas en esta cartera</p>
+                    ) : (
+                        <div>
+                            {
+                                wallet.cryptocurrencies.map((crypto) => (
+                                    <div key={crypto.id}>
+                                        <p>{crypto.name}</p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    )
+                }
             </Card>
         </div>
     )
