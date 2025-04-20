403a4c243970e48f976dda680255e9f9ba257922
Juan <juanpanizzi@hotmail.com>
2025-04-20 10:18:06 -0300

    refactor useWallet

--- src/hooks/useWallet.ts
+++ src/hooks/useWallet.ts
@@ -1,10 +1,52 @@
+import { useContext, useState } from "react";
+import { WalletContext } from "../context/walletContext";
+import { Wallet } from "../types/wallets";
 
 export const useWallet = () => { 
+
+    const { wallets, addWallet, deleteWallet } = useContext(WalletContext);
+    const [visible, setVisible] = useState<boolean>(false);
+    const [cryptos, setCryptos] = useState<Crypto[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [newWallet, setNewWallet] = useState({
+      name: '',
+      id: `${crypto.randomUUID()}`
+    });
+
+const handleLoading = (loading: boolean) => setLoading(loading);
+
+const handleVisible = (visible: boolean) => setVisible(visible);
+
+const handleCryptos = (cryptos: Crypto[]) => setCryptos(cryptos);
+
+const handleNewWallet = (newWallet: { name: string; id: string }) => setNewWallet(newWallet);
+
+
+const createWallet = () => {
+
+    if (newWallet.name === '') {
+      return false;
+    }
+    addWallet({ ...newWallet, cryptocurrencies: [], transactions: [] });
+    setVisible(false);
+    setNewWallet({ name: '', id: crypto.randomUUID() });
+    return true;
+  };
 
 
 return {
 
-    
+    wallets,
+    visible,
+    cryptos,
+    loading,
+    newWallet,
+    addWallet,
+    deleteWallet,   
+    handleVisible,
+    handleCryptos,
+    handleLoading,
+    handleNewWallet,
+    createWallet
 }
-
 }
--- src/views/Wallets.tsx
+++ src/views/Wallets.tsx
@@ -1,40 +1,33 @@
 import { Button } from "primereact/button"
 import WalletCard from "../components/Wallet/WalletCard"
-import { WalletContext } from "../context/walletContext"
-import { useContext, useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { Dialog } from "primereact/dialog";
 import { InputText } from "primereact/inputtext";
 import { Toast } from "primereact/toast";
 import { ConfirmDialog } from "primereact/confirmdialog";
 import { getCryptos } from "../services/API";
 import { ProgressSpinner } from "primereact/progressspinner";
+import { useWallet } from "../hooks/useWallet";
 export const Wallets = () => {
 
 
-  const { wallets, addWallet, deleteWallet } = useContext(WalletContext);
-  const [visible, setVisible] = useState<boolean>(false);
-  const [cryptos, setCryptos] = useState<Crypto[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [newWallet, setNewWallet] = useState({
-    name: '',
-    id: crypto.randomUUID()
-  });
+  const { wallets, deleteWallet, handleVisible, handleCryptos, handleNewWallet, handleLoading, createWallet, visible, newWallet, loading, cryptos } = useWallet();
+
+
   const toast = useRef<Toast>(null);
 
   const handleCreateWallet = () => {
-
-    if (newWallet.name === '') {
+    const success = createWallet();
+    if (!success) {
       toast.current?.show({ severity: "error", summary: "Error", detail: "El nombre de la cartera es obligatorio" });
-      return;
+      return
     }
-    addWallet({ ...newWallet, cryptocurrencies: [], transactions: [] });
-    setVisible(false);
-    setNewWallet({ name: '', id: crypto.randomUUID() });
-  };
+    toast.current?.show({ severity: "success", summary: "Operación exitosa", detail: "Cartera creada correctamente", life: 3000 });
+  }
 
   const handleCancel = () => {
-    setVisible(false);
-    setNewWallet({ name: '', id: crypto.randomUUID() });
+    handleVisible(false);
+    handleNewWallet({ name: '', id: crypto.randomUUID() });
   };
 
   const handleDeleteWallet = (id: string) => {
@@ -44,24 +37,23 @@ export const Wallets = () => {
 
   }
 
-useEffect(() => {
-
-  const cryptos = sessionStorage.getItem('cryptos');
-  if (cryptos) {
-    setCryptos(JSON.parse(cryptos));
-  } else {
-    const fetchCryptos = async () => {
-      setLoading(true);
-      const response = await getCryptos();
-      if (response.success) {
-      setCryptos(response.data);
-      sessionStorage.setItem('cryptos', JSON.stringify(response.data));
+  useEffect(() => {
+    const cryptos = sessionStorage.getItem('cryptos');
+    if (cryptos) {
+      handleCryptos(JSON.parse(cryptos));
+    } else {
+      const fetchCryptos = async () => {
+        handleLoading(true);
+        const response = await getCryptos();
+        if (response.success) {
+          handleCryptos(response.data);
+          sessionStorage.setItem('cryptos', JSON.stringify(response.data));
+        }
+        handleLoading(false);
       }
-      setLoading(false);
+      fetchCryptos();
     }
-    fetchCryptos();
-  }
-}, [])
+  }, [])
 
 
   return (
@@ -72,38 +64,38 @@ useEffect(() => {
         rejectLabel="Cancelar"
         pt={{ rejectButton: { className: 'mr-2' } }} />
       <Toast ref={toast} />
-        <Dialog
-          header="Crear Nueva Cartera"
-          visible={visible}
-          className="w-[90vw] md:w-[50vw] lg:w-[40vw] xl:w-[30vw]"
-          onHide={() => setVisible(false)}
-          footer={
-            <div>
-              <Button label="Cancelar" icon="pi pi-times" onClick={handleCancel} className="p-button-text" />
-              <Button label="Crear" icon="pi pi-check" onClick={handleCreateWallet} autoFocus />
-            </div>
-          }
-        >
-          <div className="p-fluid">
-            <div className="field">
-              <label htmlFor="name">Nombre de la Cartera</label>
-              <InputText
-                id="name"
-                value={newWallet.name}
-                onChange={(e) => setNewWallet({ ...newWallet, name: e.target.value })}
-                placeholder="Ingresa el nombre de la cartera"
-              />
-            </div>
+      <Dialog
+        header="Crear Nueva Cartera"
+        visible={visible}
+        className="w-[90vw] md:w-[50vw] lg:w-[40vw] xl:w-[30vw]"
+        onHide={() => handleVisible(false)}
+        footer={
+          <div>
+            <Button label="Cancelar" icon="pi pi-times" onClick={handleCancel} className="p-button-text" />
+            <Button label="Crear" icon="pi pi-check" onClick={handleCreateWallet} autoFocus />
           </div>
-        </Dialog>
-      
+        }
+      >
+        <div className="p-fluid">
+          <div className="field">
+            <label htmlFor="name">Nombre de la Cartera</label>
+            <InputText
+              id="name"
+              value={newWallet.name}
+              onChange={(e) => handleNewWallet({ ...newWallet, name: e.target.value })}
+              placeholder="Ingresa el nombre de la cartera"
+            />
+          </div>
+        </div>
+      </Dialog>
+
       {loading && <div className="flex justify-center items-center h-screen">
-          <ProgressSpinner />
+        <ProgressSpinner />
       </div>}
       {!loading && cryptos.length > 0 && <section>
         <div className="flex w-full items-center justify-between p-2">
           <h1 className="text-4xl ">Carteras</h1>
-          <Button label="Crear Cartera" icon="pi pi-plus" onClick={() => setVisible(true)} />
+          <Button label="Crear Cartera" icon="pi pi-plus" onClick={() => handleVisible(true)} />
         </div>
 
         <div className="flex flex-wrap gap-10 items-start xl:mt-10 mx-5 place-items-center ">
@@ -116,7 +108,7 @@ useEffect(() => {
       {!loading && wallets.length === 0 && <div className="flex justify-center items-center h-screen">
         <h1 className="text-4xl ">No se registran carteras en este momento, crea una para empezar a operar</h1>
       </div>}
-    
+
     </>
   )
 }
