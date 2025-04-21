6274a8ccaa6dc5d3f58735de178fdced4dac8def
Juan <juanpanizzi@hotmail.com>
2025-04-21 12:33:14 -0300

    adv styles

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -19,12 +19,20 @@ export const Layout = () => {
     },
 
   ];
-  
+
   
   return (
     <>
-    <Menubar model={items} />
-    <Outlet />
+    <div className="min-h-screen flex flex-col "> 
+      <Menubar model={items} className="shadow-lg mx-4 bg-[#14151B]" pt={{
+        menuitem: {
+          className: 'hover:bg-[#282936] rounded-sm'
+        }
+      }} />
+        <main className=" flex-1 p-4  m-4 h-full min-h-[calc(100vh-7rem)] rounded-lg bg-[#14151B] border-[1px] border-[#3E4053]">
+          <Outlet />
+        </main>
+    </div>
     </>
   )
 }
