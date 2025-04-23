668db6a16886b201c3f1aac3c066f538155a79c6
Juan <juanpanizzi@hotmail.com>
2025-04-22 23:47:47 -0300

    refactor service api

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const API_KEY = 'CG-MX4Unb5kP7XqRvxDnBrRD2PA';
-// const BASE_URL = "https://api.coingecko.com/api/v3";
+
 const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = import.meta.env.VITE_BASE_API_URL;
 
@@ -12,7 +11,6 @@ export const getCryptos = async () => {
       params: { vs_currency: 'usd' },
       headers: { 'x-cg-demo-api-key': API_KEY }
     });
-    console.log('response', response.data);
     return { success: true, data: response.data };
   } catch (error) {
     console.log('error', error);
