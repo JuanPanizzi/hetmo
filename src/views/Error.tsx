484736f412bca84c5b882698c5f1f37eb84b1010
Juan <juanpanizzi@hotmail.com>
2025-04-23 09:11:46 -0300

    responsive error message

--- src/views/Error.tsx
+++ src/views/Error.tsx
@@ -5,11 +5,10 @@ export const Error = ({ message }: { message: string }) => {
     const navigate = useNavigate();
     return (
         <>
-    <div className="flex flex-col justify-center items-center h-screen gap-4">
-        <i className="pi pi-exclamation-triangle text-purple-500 text-8xl"></i>
-        <h1 className="text-4xl font-bold text-gray-800">{message}</h1>
-            {/* <p className="text-xl text-gray-600">{message}</p> */}
-            <Button label="Volver a inicio" icon="pi pi-arrow-left" onClick={() => navigate('/')} />
+    <div className="flex flex-col justify-center items-center h-screen gap-4 px-4">
+        <i className="pi pi-exclamation-triangle text-purple-500 text-6xl md:text-8xl"></i>
+        <h1 className="text-2xl md:text-4xl font-bold text-gray-800 text-center">{message}</h1>
+            <Button label="Volver a inicio" icon="pi pi-arrow-left" onClick={() => navigate('/')} className="w-full md:w-auto max-w-[300px]" />
         </div>
     </>
   )
