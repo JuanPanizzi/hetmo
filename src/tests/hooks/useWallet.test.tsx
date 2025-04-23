b861a2df8aa8e2961ff4c6d6ecf42bd33189ed4e
Juan <juanpanizzi@hotmail.com>
2025-04-23 20:25:14 -0300

    test handleNewWallet on useWallet hook test

--- src/tests/hooks/useWallet.test.tsx
+++ src/tests/hooks/useWallet.test.tsx
@@ -1,55 +1,72 @@
-import { render, screen, fireEvent } from "@testing-library/react"
-import { MemoryRouter } from "react-router-dom"
-import { Wallets } from "../../views/Wallets"
-import { Wallet } from "../../types/wallets"
-import { HeaderCard } from "../../components/UI/HeaderCard"
-import { WalletModal } from "../../components/Wallet/WalletModal"
-import userEvent from "@testing-library/user-event" 
-import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
+// tests/hooks/useWallet.test.tsx
+import { renderHook, act } from '@testing-library/react'
+import React from 'react'
+import { WalletContext } from '../../context/walletContext'
+import { useWallet } from '../../hooks/useWallet'
+import { vi, describe, it, expect, beforeEach } from 'vitest'
 
-describe('useWallet', () => {
-    const saveWallet = vi.fn();
-
-    beforeEach(() => {
-        render(
-            <MemoryRouter>
-                <Wallets />
-                <WalletModal
-                    showWalletModal={true}
-                    handleWalletModal={() => {}}
-                    handleCancel={() => {}}
-                    handleSaveWallet={saveWallet}
-                    newWallet={{name: 'Wallet 1', id: '1'}}
-                    handleNewWallet={() => {}}
-                    selectedWallet={undefined}
-                />
-
-                <HeaderCard
-                    title="Carteras"
-                    buttonLabel="Crear Cartera"
-                    onButtonClick={() => {}}
-                />
-                <Wallets />
-            </MemoryRouter>
-        );
-    });
-
-    it('should add a new wallet', async () => {
-        
-        const walletTest= {id: '1', name: 'Wallet 1' };
-        
-        const inputWalletName = screen.getByTestId('wallet-name-input');
-        
-        await userEvent.type(inputWalletName, walletTest.name);
-        
-        const addWalletButton = screen.getByTestId('save-wallet-button');
-        
-        fireEvent.click(addWalletButton);
-        
-        expect(saveWallet).toHaveBeenCalled();
-        expect(screen.getByDisplayValue('Wallet 1')).toBeInTheDocument();
-        expect(screen.getByRole('dialog')).toHaveTextContent('Crear Nueva Cartera');
-    });
-});
+describe('useWallet hook', () => {
+  
+// 
+  let fakeAdd: ReturnType<typeof vi.fn>
+  let fakeUpdate: ReturnType<typeof vi.fn>
+  let fakeDelete: ReturnType<typeof vi.fn>
+  let fakeAddTransaction: ReturnType<typeof vi.fn>
+  let fakeDeleteTransaction: ReturnType<typeof vi.fn>
+  let fakeEditTransaction: ReturnType<typeof vi.fn>
+  let fakeUpdateTransactionStatus: ReturnType<typeof vi.fn>
 
 
+  const wrapper = ({ children }: { children: React.ReactNode }) => (
+    <WalletContext.Provider
+      value={{
+        wallets: [],       
+        addWallet: fakeAdd,
+        updateWallet: fakeUpdate,
+        deleteWallet: fakeDelete,
+        addTransaction: fakeAddTransaction,
+        deleteTransaction: fakeDeleteTransaction,
+        editTransaction: fakeEditTransaction,
+        updateTransactionStatus: fakeUpdateTransactionStatus
+      }}
+    >
+      {children}
+    </WalletContext.Provider>
+  )
+
+  beforeEach(() => {
+    fakeAdd = vi.fn()
+    fakeUpdate = vi.fn()
+    fakeDelete = vi.fn()
+    fakeAddTransaction = vi.fn()
+    fakeDeleteTransaction = vi.fn()
+    fakeEditTransaction = vi.fn()
+    fakeUpdateTransactionStatus = vi.fn()
+  })
+
+  it('should initialize default states of useWallet custom hook', () => {
+    const { result } = renderHook(() => useWallet(), { wrapper })
+
+    expect(result.current.showWalletModal).toBe(false)
+    expect(result.current.isEditing).toBe(false)
+    expect(result.current.newWallet.name).toBe('')
+    expect(typeof result.current.newWallet.id).toBe('string')
+    expect(result.current.cryptos).toEqual([])
+    expect(result.current.loading).toBe(false)
+    expect(result.current.selectedWallet).toBeUndefined()
+  })
+
+  
+    it('should add a new wallet with handleNewWallet ', () => {
+      const { result } = renderHook(() => useWallet(), { wrapper })
+
+      result.current.isEditing = false;
+      act(() => {
+        result.current.handleNewWallet({ id:'1', name:'New Wallet test' })
+      })
+      expect(result.current.newWallet.name).toBe('New Wallet test')
+  })
+
+
+
+})
