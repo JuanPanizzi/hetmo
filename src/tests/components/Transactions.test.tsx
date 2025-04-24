5de0de2c9c04dfff1f2063ca306ead732b223225
Juan <juanpanizzi@hotmail.com>
2025-04-24 13:29:03 -0300

    updates test for transaction modal

--- src/tests/components/Transactions.test.tsx
+++ src/tests/components/Transactions.test.tsx
@@ -1,9 +1,11 @@
 import { Transactions } from "../../views/Transactions"
 import { describe, it, expect, beforeEach, vi } from 'vitest';
-import { render, screen } from "@testing-library/react"
+import { render, screen, fireEvent } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom"
 import { useTransactions } from "../../hooks/useTransactions";
-
+import { TransactionModal } from "../../components/WalletDetail/TransactionModal";
+import { PrimeReactProvider } from 'primereact/api';
+import '../../i18n/primeReactLocale';
 
 vi.mock('../../hooks/useTransactions', () => ({
     useTransactions: vi.fn()
@@ -68,13 +70,36 @@ describe('Transactions', () => {
     })  
 
     it('should render the pending transactions component when wallet exists', () => {
-
         const wallet = walletTest
         wallet.transactions[0].status = 'confirmada'
         expect(screen.getByText('Transacciones pendientes')).toBeInTheDocument()
         expect(screen.getByText('Confirmar')).toBeInTheDocument()
     })
 
-   
+    it('should render TransactionModal when clicking "Nueva Transacción" button', () => {
+        
+        render(
+            <PrimeReactProvider value={{ locale: 'es' }}>
+                <MemoryRouter>
+                    <TransactionModal 
+                        visible={true} 
+                        handleSetVisible={vi.fn()} 
+                        newTransaction={walletTest.transactions[0]} 
+                        handleNewTransaction={vi.fn()} 
+                        cryptos={[]} 
+                        handleCancel={vi.fn()} 
+                        saveNewTransaction={vi.fn()} 
+                        isEditing={false} 
+                    />
+                </MemoryRouter>
+            </PrimeReactProvider>
+        )
 
+        const newTransactionButton = screen.getByRole('button', { name: /Nueva Transacción/i });
+        expect(newTransactionButton).toBeInTheDocument();
+
+        fireEvent.click(newTransactionButton);
+
+        expect(screen.getByRole('dialog')).toBeInTheDocument();
+    });
 })
