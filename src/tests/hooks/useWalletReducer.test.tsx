3b58a77769bc0fd81c6ca5152a3ee4761a86c188
Juan <juanpanizzi@hotmail.com>
2025-04-23 22:16:08 -0300

    adv test add transaction

--- src/tests/hooks/useWalletReducer.test.tsx
+++ src/tests/hooks/useWalletReducer.test.tsx
@@ -2,6 +2,7 @@ import { WalletContext } from "../../context/walletContext"
 import { act, renderHook } from "@testing-library/react"
 import { useWalletsReducer } from "../../hooks/useWalletsReducer"
 import { describe, it, expect, vi } from 'vitest'
+import { Transaction, Wallet } from "../../types/wallets"
 
 describe('useWalletReducer', () => {
     const wrapper = ({ children }: { children: React.ReactNode }) => (
@@ -56,6 +57,40 @@ describe('useWalletReducer', () => {
         expect(result.current.wallets).not.toContain(walletTest)        
     })
 
+    it('should add transaction', () => {
+        const { result } = renderHook(() => useWalletsReducer(), { wrapper })
 
+        const walletTest = { 
+            id: '1', 
+            name: 'New Wallet test', 
+            cryptocurrencies: [], 
+            transactions: [] 
+        }
+
+        const transactionTest: Transaction = { 
+            id: '1', 
+            type: 'Compra', 
+            crypto: { id: '1', name: 'Bitcoin', symbol: 'BTC', image: '', current_price: 10000 }, 
+            amount: 1, 
+            price: 10000, 
+            date: new Date().toISOString(), 
+            status: 'pendiente'
+           
+        }
+
+        act(() => {
+            result.current.addWallet(walletTest)
+        })
+
+        act(() => {
+            result.current.addTransaction(walletTest.id, transactionTest)
+        })
+
+        const walletWithTransaction = result.current.wallets.find((wallet: Wallet) => wallet.id === walletTest.id)
+        expect(walletWithTransaction?.transactions).toContainEqual(
+            expect.objectContaining(transactionTest) 
+          );
+          
+    })  
 })
 
