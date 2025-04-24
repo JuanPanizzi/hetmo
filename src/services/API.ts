47d641101d52cf6b2c7badd14b11240cf094eb7b
Juan <juanpanizzi@hotmail.com>
2025-04-24 09:28:17 -0300

    updates service

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,15 +1,12 @@
 import axios from "axios";
 
 
-const API_KEY = import.meta.env.VITE_API_KEY;
-const BASE_URL = import.meta.env.VITE_BASE_API_URL;
 
 export const getCryptos = async () => {
 
   try {
-    const response = await axios.get(`${BASE_URL}/coins/markets`, {
+    const response = await axios.get(`https://api.coingecko.com/api/v3/coins/markets`, {
       params: { vs_currency: 'usd' },
-      headers: { 'x-cg-demo-api-key': API_KEY }
     });
     return { success: true, data: response.data };
   } catch (error) {
